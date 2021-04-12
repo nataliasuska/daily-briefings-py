efd1e8b7ffe1d739161243a5001ef1bda99f964e
Natalia Suska <74929794+nataliasuska@users.noreply.github.com>
2021-04-12 18:14:27 -0400

    Updates routes

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
--- web.app/home_routes.py
+++ web.app/home_routes.py
@@ -16,5 +16,8 @@ def about():
 @home_routes.route("/hello")
 def hello_world():
     print("HELLO", dict(request.args))
+    # NOTE: `request.args` is dict-like, so below we're using the dictionary's `get()` method,
+    # ... which will return None instead of throwing an error if key is not present
+    # ... see also: https://www.w3schools.com/python/ref_dictionary_get.asp
     name = request.args.get("name") or "World"
     return f"Hello, {name}!"
